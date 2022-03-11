fa97d140c49fa804b3dc24637d576369e5ea23cd
domingosl <ing.domingo.sl@gmail.com>
2022-03-11 17:37:57 +0100

    Added edges in response

--- api/controllers/nodes/get.js
+++ api/controllers/nodes/get.js
@@ -6,22 +6,40 @@ new utilities.express
     .respondsAt('/nodes/:id')
     .controller(async (req, res) => {
 
-        let response = await dbSession.run('MATCH (n) WHERE id(n) = $id RETURN n LIMIT 1', { id: parseInt(req.params.id) });
+        let response = await dbSession.run('MATCH (node) WHERE id(node) = $id RETURN node LIMIT 1', {id: parseInt(req.params.id)});
 
-        if(response.records.length === 0)
+        if (response.records.length === 0)
             return res.resolve();
 
-        const obj = response.records[0].toObject();
 
-            const isProblem = obj.n.labels.indexOf('PROBLEM') >= 0;
+        const nodeObj = response.records[0].toObject();
 
-            res.resolve({
-                id: obj.n.identity.toNumber(),
-                name: obj.n.properties.name,
-                content: obj.n.properties.content || "{}",
-                color: "#efb725",
-                type: isProblem ? 0 : 1, //0: problem
-                group: isProblem ? 0 : 1
+        const isProblem = nodeObj.node.labels.indexOf('PROBLEM') >= 0;
+
+
+        response = await dbSession.run('MATCH (node)-[edge]-() WHERE id(node) = $id RETURN edge', {id: parseInt(req.params.id)});
+
+        const edges = [];
+        response.records.forEach(r => {
+
+            const edgeObj = r.toObject().edge;
+
+            edges.push({
+                from: edgeObj.start.toNumber(),
+                to: edgeObj.end.toNumber(),
+                type: edgeObj.type
             });
+
+        })
+
+        res.resolve({
+            id: nodeObj.node.identity.toNumber(),
+            name: nodeObj.node.properties.name,
+            content: nodeObj.node.properties.content || "{}",
+            color: "#efb725",
+            type: isProblem ? 0 : 1, //0: problem
+            group: isProblem ? 0 : 1,
+            edges
+        });
 
     });
